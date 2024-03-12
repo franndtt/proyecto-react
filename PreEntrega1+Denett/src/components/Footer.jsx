71cb476033c0b36711d94117cf30fbfca14c9112
KernelOS <francodenettok@gmail.com>
2024-03-12 16:07:48 -0300

    odificacion de footer

--- PreEntrega1+Denett/src/components/Footer.jsx
+++ PreEntrega1+Denett/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <div className="container-fluid bg-dark fixed-bottom ">
+      <div className="container-fluid bg-dark fixed-bottom p-4">
         <div className="row">
             <div className="col">
                 <p className="text-center text-light">Todos los derechos reservados</p>
